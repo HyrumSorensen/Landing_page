6a6bad5743965afa402656967ead46a963778472
Hyrum Sorensen <hyrumgary@gmail.com>
2022-06-30 15:10:29 -0600

    added intentional error

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const path = require('path')
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '67f9782632344b44ae19e3802f4bb89e',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!")
+
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+})
+app.get('/', (req, res) => {
+    functionThatExist();
 })
 
 app.get('/css', (req, res) => {
